031a424c5b5d9bf65cafbcf709eefa4e571d1a9f
alvizeta <a.zorzini@gmail.com>
2020-06-04 14:18:20 -0300

    Updates

---
+++ precache-manifest.c825d1354ed51550d004035cc3431dc8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf2ee33aef9a21fe9497bc9f6ecb043f",
+    "url": "/coronapp/index.html"
+  },
+  {
+    "revision": "88e1a51baabf4743a357",
+    "url": "/coronapp/static/js/2.164761aa.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/coronapp/static/js/2.164761aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa85db508e7f657ae8f5",
+    "url": "/coronapp/static/js/main.43056744.chunk.js"
+  },
+  {
+    "revision": "9ed725cfae397f8cd9c1",
+    "url": "/coronapp/static/js/runtime-main.380e3a05.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/coronapp/static/media/image.d7265326.png"
+  }
+]);
